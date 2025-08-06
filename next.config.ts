29db4190a85c30ad0ee662d16c5ade9f241d0166
KateSt421 <belporibet@mail.ru>
2025-08-06 19:07:55 +0300

    fix: change config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Генерация статического HTML
-  basePath: process.env.NODE_ENV === 'production' ? '/school-test' : '', // Замените на имя вашего репозитория
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/school-test/' : '', // То же самое здесь
 };
 
 export default nextConfig;
